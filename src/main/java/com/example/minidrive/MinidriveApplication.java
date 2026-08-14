aafa2bb08a9779bcd6c3f91656d199277ab4740c
Damini-06 <25110cn581@glbitm.ac.in>
2026-08-14 09:08:32 +0530

    1st commit

---
+++ src/main/java/com/example/minidrive/MinidriveApplication.java
@@ -0,0 +1,13 @@
+package com.example.minidrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinidriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinidriveApplication.class, args);
+	}
+
+}
